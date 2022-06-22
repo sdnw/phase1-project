db44af1dd22a8c0fdd542ebe1336c655c2ce1d9c
Steven <stevendawn98@gmail.com>
2022-06-22 14:06:42 -0400

    mvp?

--- index.js
+++ index.js
@@ -1,107 +1,74 @@
 // write your code here
-
 document.querySelector("#comments-list").innerHTML=""
-
-
-
 fetch("https://api.opensea.io/api/v1/assets?format=json")
 .then(resp => resp.json())
 .then(data => renderImage(data.assets))
 // .then(data => renderpic(data))
-
 function renderImage (data) {
     data.forEach(element => {notforrender(element)})}
-
 function notforrender (element) {
-
-    if(element.image_url == null ) { 
-
+    if(element.image_url == null ) {
     }
-    
     else {
     let list=document.querySelector(".image-container")
-    let div=document.createElement("div")        
+    let div=document.createElement("div")
     let img=document.createElement("img")
     let name=document.createElement("h2")
     let likes=document.createElement("p")
-     
-    const detailImage = document.querySelector('.detail-image')       
-    
         img.src=element.image_url
         name.innerText=element.name.slice(0,12)
-        likes.innerText=element.token_id*.0000000000000000000000000000000000000000000000000000000000000000000000001
+        likes.innerText=Math.trunc(element.token_id*.0000000000000000000000000000000000000000000000000000000000000000000000001)
         name.className= "title"
         img.className = "image"
         div.className = "image-card"
-
-        detailImage.src = "element.image_url"                
-    
     let btnup=document.createElement("button")
     let btndown= document.createElement("button")
-    
-
         btnup.innerText="Upvote"
         btndown.innerText="Downvote"
-       
         btnup.className="float-child"
         btndown.className="float-child"
-        
-    
     let test=[img,name,likes]
     // console.log(test[2])
-
-    img.addEventListener('mouseover', (e) => {
-
-        console.log('mouseovered')
-    })
-    
-    div.append(img,name,btnup,btndown)
+    div.append(img,name,likes,btnup,btndown)
     list.append(div)
-
     btnup.addEventListener("click", () => {
         let oldlikes =parseInt(likes.innerText)
         let newlikes=oldlikes +1
         likes.innerText = newlikes
         console.log(newlikes)
     })
-
     btndown.addEventListener("click", () => {
         let oldlikes =parseInt(likes.innerText)
         let newlikes=oldlikes -1
         likes.innerText = newlikes
-        console.log(newlikes)        
-        
+        console.log(newlikes)
     })
 
-    // // highlight the mouseover target 
-
-    // event.target.style.width = `150%`
-    // event.target.style.height= `150%`
-
+    // hover
+    // img.addEventListener("mouseover", function( event ) {
+    // // highlight the mouseover target
+    // event.target.style.width = `125%`
+    // event.target.style.height= `125%`
     // // reset the color after a short delay
     // setTimeout(function() {
-    //     event.target.style.width = `100%`;
-    //     event.target.style.height= `100%`
+    //     event.target.style.width = `70%`;
+    //     event.target.style.height= `70%`
     // }, 500);
     // }, false);
-
     };
+        }
+        const form = document.querySelector('#new-nft-form').addEventListener('submit', submitNft)
+        function submitNft(event) {
+                event.preventDefault()
+                const name = document.querySelector('#nft-name').value
                 console.log(name)
-                
                 const image = document.querySelector('#nft-image').value
                 console.log(image)
-        
                 let newNft = {
                     'name': name,
                     'image_url': image,
+                    'token_id': "0"
                     }
                     console.log(newNft)
                     notforrender(newNft)
                 }
-
-
-        
-
-
-
-                                                   
